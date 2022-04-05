c59228a2c63d579dfb4da6178f8cd46c7debf550
Victor Rentea <victorrentea@gmail.com>
2022-04-05 10:29:59 +0300

    Grouping FLuxes

--- spring-webflux/src/main/java/victor/training/reactive/usecase/grouping/GroupingFluxes.java
+++ spring-webflux/src/main/java/victor/training/reactive/usecase/grouping/GroupingFluxes.java
@@ -4,14 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Slf4j
 public class GroupingFluxes {
    public static void main(String[] args) {
-      //Depending on the message type, run one of the following flows:
-      //TYPE1: Do nothing (ignore the message)
-      //TYPE2: Call apiA(id) and apiB(id) in parallel
-      //TYPE3: Call apiC(idList), buffering together requests such that
-      //you send max 3 IDs, an ID waits max 500 millis
 
       Flux<Integer> messageStream =
           Flux.range(0, 10);
@@ -26,10 +23,26 @@ public class GroupingFluxes {
    }
 
    public Mono<Void> processMessageStream(Flux<Integer> messageStream) {
+      //Depending on the message type, run one of the following flows:
+      //TYPE1: Do nothing (ignore the message)
+      //TYPE2: Call apiA(id) and apiB(id) in parallel
+      //TYPE3: Call apiC(idList), buffering together requests such that
+      //you send max 3 IDs, an ID waits max 500 millis
       return messageStream
+
+          .groupBy(m -> MessageType.forMessage(m))
+          .flatMap(groupedFlux -> {
+             switch (groupedFlux.key()) {
+                case TYPE1_NEGATIVE: return Mono.empty();
+                case TYPE2_ODD: return groupedFlux.flatMap(m2 -> Mono.zip(apis.apiA(m2), apis.apiB(m2)));
+                case TYPE3_EVEN: return groupedFlux
+                    .bufferTimeout(3, Duration.ofMillis(500))
+                    .flatMap(pageOfType3 -> apis.apiC(pageOfType3));
+                default:
+                   return Flux.error(new IllegalStateException("Unexpected value: " + groupedFlux.key()));
+             }
+          })
           .then() // starting point
-
-
           ;
    }
 
