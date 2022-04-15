3117430830954cc12cbf6cc28bf5bf87b3a70668
Victor Rentea <victorrentea@gmail.com>
2022-04-15 13:39:35 +0300

    END

--- spring-webflux/src/main/java/victor/training/reactive/usecase/monitoringinfinite/MigratingIMperative.java
+++ spring-webflux/src/main/java/victor/training/reactive/usecase/monitoringinfinite/MigratingIMperative.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import victor.training.reactive.Utils;
 
 import java.util.Collections;
@@ -24,7 +25,18 @@ public static void main(String[] args) {
 @GetMapping
    public Mono<String> method() {
       return yourCode("id")
-          .map(s -> legacy.innocent(s));
+          .flatMap(s -> wrappingLegacyBlockingCode(s));
+   }
+
+   public Mono<String> wrappingLegacyBlockingCode(String s) {
+   // in a method returning Mono/Flux you ARE NOT ALLOWED TO :
+      // block or throw exceptions.
+      return Mono.fromSupplier(() -> legacy.innocent(s))
+          .log("Above")
+          .subscribeOn(Schedulers.boundedElastic())
+          .log("Bellow")
+//          .flatMap(a -> fff(a))
+          ;
    }
 
    private Mono<String> yourCode(String s) {
