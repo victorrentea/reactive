0bf656eca508e2adacaa6dbf69d25cb8d2346da2
victorrentea <victorrentea@gmail.com>
2022-07-27 23:06:51 +0300

    yolo

--- spring-webflux/src/main/java/victor/training/reactive/usecase/complex/TimedReactiveAspect.java
+++ spring-webflux/src/main/java/victor/training/reactive/usecase/complex/TimedReactiveAspect.java
@@ -9,7 +9,9 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Timed;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -24,22 +26,26 @@ public class TimedReactiveAspect {
     }
 
     @Autowired
-    private MeterRegistry simpleMeterRegistry;
+    private MeterRegistry meterRegistry;
 
     @Around("@annotation(victor.training.reactive.usecase.complex.TimedReactiveAspect.TimedReactive)")
     public Object method(ProceedingJoinPoint pjp) throws Throwable {
-        String metricName = pjp.getSignature().getName()+"-elapsed";
+        String metricName = pjp.getSignature().getName() + "-elapsed";
 
-        Timer timer = simpleMeterRegistry.timer(metricName);
-        timer.record(Duration.ofMillis(100));
+        Timer timer = meterRegistry.timer(metricName);
+        timer.record(Duration.ofMillis(100)); // example: find it in /actuator/prometheus TODO remove me
 
-        Object ret = pjp.proceed();
-        if (ret instanceof Mono<?>) {
-            Mono<?> mono = (Mono<?>) ret;
-            // TODO measure and timer.record(...) the time elapsed between subscribe and next signals
+        Object result = pjp.proceed();
+        if (result instanceof Mono<?>) {
+            Mono<?> mono = (Mono<?>) result;
+            // TODO #1 measure and timer.record(...) the time elapsed between subscribe and next signals
             return mono;
             // SOLUTION: return mono.timed().doOnNext(timed -> timer.record(timed.elapsedSinceSubscription())).map(Timed::get);
+        } else if (result instanceof Flux<?>) {
+            Flux<?> flux = (Flux<?>) result;
+            // TODO #2 measure time elapsed between subscribe and completion signals (Hint: .timed() won't help anymore)
+            throw new IllegalArgumentException("not implemented ");
         }
-        return ret;
+        return result;
     }
 }
