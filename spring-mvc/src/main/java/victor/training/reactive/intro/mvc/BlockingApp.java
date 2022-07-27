40adc9b86642faac28bfc401c050e350d502b56c
victorrentea <victorrentea@gmail.com>
2022-07-27 20:48:15 +0300

    polish

--- spring-mvc/src/main/java/victor/training/reactive/intro/mvc/BlockingApp.java
+++ spring-mvc/src/main/java/victor/training/reactive/intro/mvc/BlockingApp.java
@@ -12,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 
 import static java.lang.System.currentTimeMillis;
@@ -26,8 +27,8 @@ public class BlockingApp {
 
    @Bean
    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-      return registry -> registry.config()
-          .commonTags(
+      // expose tomcat metrics (threads, connections) to prometheus via /actuator/prometheus
+      return registry -> registry.config().commonTags(
               "application", "spring-mvc",
               "region", "training-region");
    }
@@ -41,7 +42,7 @@ public class BlockingApp {
    private Barman barman;
 
    @GetMapping("fast")
-   public String fast() throws Exception {
+   public String fast() {
       return "immediate";
    }
 
@@ -52,11 +53,13 @@ public class BlockingApp {
       long t0 = currentTimeMillis();
 
       Beer beer = barman.pourBeer();
+
       Vodka vodka = barman.pourVodka();
 
-      log.debug("HTTP thread was blocked for {} millis ", (currentTimeMillis() - t0));
+      DillyDilly dilly = new DillyDilly(beer, vodka);
 
-      return new DillyDilly(beer, vodka);
+      log.debug("HTTP thread was blocked for {} millis ", (currentTimeMillis() - t0));
+      return dilly;
    }
 }
 
@@ -65,27 +68,25 @@ class Barman {
    private static final Logger log = LoggerFactory.getLogger(Barman.class);
 
    public Beer pourBeer() {
-      log.info("Start pour beer");
+      log.info("Start beer");
 
-      // 1: emulate
-//      sleep(1000);
-//      Beer beer = new Beer("blond");
+      // 1: pretend
+      sleep(1000);
+      Beer beer = new Beer("blond");
 
-      // 2: really call
-      Beer beer = new RestTemplate().getForEntity("http://localhost:9999/api/beer", Beer.class).getBody();
-
-      // 3: async alternative
-//      return new AsyncRestTemplate().exchange()
-//          .completable().thenApply(tranform);
-
-      log.info("End pour beer");
+      // 2: blocking REST call
+//      Beer beer = new RestTemplate().getForEntity("http://localhost:9999/api/beer", Beer.class).getBody();
+      log.info("End beer");
       return beer;
+
+      // 3: non-blocking REST call
+//      return new AsyncRestTemplate().exchange(...).completable()....;
    }
 
    public Vodka pourVodka() {
-      log.info("Start pour vodka");
+      log.info("Start vodka");
       sleep(200);  // imagine blocking DB call
-      log.info("End pour vodka");
+      log.info("End vodka");
       return new Vodka();
    }
 }
@@ -94,10 +95,9 @@ class Barman {
 class Beer {
    private String type;
    public Beer() {}
-   Beer(String type) {
+   public Beer(String type) {
       this.type = type;
    }
-
    public String getType() {
       return type;
    }
@@ -105,7 +105,6 @@ class Beer {
 
 class Vodka {
    private final String type = "deadly";
-
    public String getType() {
       return type;
    }
