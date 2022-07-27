8cfb82a68af8dad708a1e8f8c432044b768a5450
victorrentea <victorrentea@gmail.com>
2022-07-27 20:55:15 +0300

    polish

--- spring-webflux/src/main/java/victor/training/reactive/usecase/complex/ComplexFlowApp.java
+++ spring-webflux/src/main/java/victor/training/reactive/usecase/complex/ComplexFlowApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -26,87 +27,84 @@ import static victor.training.reactive.Utils.installBlockHound;
 @SpringBootApplication
 public class ComplexFlowApp implements CommandLineRunner {
 
-   public static final WebClient WEB_CLIENT = WebClient.create();
+    public static final WebClient WEB_CLIENT = WebClient.create();
 
-   public static void main(String[] args) {
-      SpringApplication.run(ComplexFlowApp.class, "--server.port=8081");
-   }
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexFlowApp.class, "--server.port=8081");
+    }
 
-   @EventListener(ApplicationStartedEvent.class)
-   public void setupBlockingDetection() {
-      installBlockHound(List.of(
-          Tuples.of("io.netty.resolver.HostsFileParser", "parse"),
-          Tuples.of("victor.training.reactive.reactor.complex.ComplexFlowMain", "executeAsNonBlocking")
-      ));
-   }
+    private static void auditResealed(Product p) {
+        if (p.isResealed()) {
+            ExternalAPIs.auditResealedProduct(p)
+                    .subscribe(Utils::noop, Utils::handleError);
+        }
+    }
 
-   @Override
-   public void run(String... args) throws Exception {
+    // perfect
+    private static Flux<Product> retrieveMany(List<Long> productIds) {
+        log.info("Call pentru " + productIds);
+        return WEB_CLIENT
+                .post()
+                .uri("http://localhost:9999/api/product/many")
+                .bodyValue(productIds)
+                .retrieve()
+                .bodyToFlux(ProductDetailsResponse.class) // jackson parseaza progresiv JSONu cum vine si-ti emite semnale de date ProductDetails.
+                .map(ProductDetailsResponse::toEntity)
+                ;
+    }
 
-      Hooks.onOperatorDebug(); // provide better stack traces
+    @EventListener(ApplicationStartedEvent.class)
+    public void setupBlockingDetection() {
+        installBlockHound(List.of(
+                Tuples.of("io.netty.resolver.HostsFileParser", "parse"),
+                Tuples.of("victor.training.reactive.reactor.complex.ComplexFlowMain", "executeAsNonBlocking")
+        ));
+    }
 
-      log.info("Calling myself automatically once");
-      WEB_CLIENT.get().uri("http://localhost:8081/complex").retrieve().bodyToMono(String.class)
-          .subscribe(
-              data -> log.info("COMPLETED with: "+data),
-              error -> log.error("FAILED! See above why" )
-          );
+    // TIP: Caching in Reactor: https://stackoverflow.com/questions/48156424/spring-webflux-and-cacheable-proper-way-of-caching-result-of-mono-flux-type
+    // ================================== work below ====================================
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Hooks.onOperatorDebug(); // provide better stack traces
+
+        log.info("Calling myself automatically once");
+        WEB_CLIENT.get().uri("http://localhost:8081/complex").retrieve().bodyToMono(String.class)
+                .subscribe(
+                        data -> log.info("COMPLETED with: " + data),
+                        error -> log.error("FAILED! See above why")
+                );
 //      startRefreshWireMockStubsFromJsonContinuously();
-   }
+    }
 
-   @GetMapping("complex")
-   public Mono<String> executeAsNonBlocking() {
-      List<Long> productIds = LongStream.rangeClosed(1, 1000_000).boxed().collect(toList());
+    @GetMapping("complex")
+    public Mono<String> executeAsNonBlocking(@RequestParam(value = "n", defaultValue = "1000") int n) {
+        List<Long> productIds = LongStream.rangeClosed(1, n).boxed().collect(toList());
 
+        return mainFlow(productIds)
+                .collectList()
+                .map(list -> "Done. Got " + list.size() + " products: " + list);
+    }
 
-      return mainFlow(productIds)
-           .collectList()
-          .map(list -> "Done. Got " + list.size() + " products: " + list);
-   }
-
-   // TIP: Caching in Reactor: https://stackoverflow.com/questions/48156424/spring-webflux-and-cacheable-proper-way-of-caching-result-of-mono-flux-type
-   // ================================== work below ====================================
-
-
-   public void jobuLansatDinQuartzUnLibVechicareNUStieReact() {
+    public void jobuLansatDinQuartzUnLibVechicareNUStieReact() {
 //      ......subsccribe(); // 1ms fara erori.
-      // m-as gandi la .block(); pt ca sa-i arunc exceptie, sa vada cat a durat.
-      // nici nu starvezi =threaduri ca are propriul thraed poolq
-   }
-   
-   public Flux<Product> mainFlow(List<Long> productIds) {
-       return Flux.fromIterable(productIds)
-              .buffer(2)
-              .flatMap(ComplexFlowApp::retrieveMany, 10)
+        // m-as gandi la .block(); pt ca sa-i arunc exceptie, sa vada cat a durat.
+        // nici nu starvezi =threaduri ca are propriul thraed poolq
+    }
+
+    public Flux<Product> mainFlow(List<Long> productIds) {
+        return Flux.fromIterable(productIds)
+                .buffer(2)
+                .flatMap(ComplexFlowApp::retrieveMany, 10)
 //              .delayUntil(ComplexFlowApp::auditResealed)
-               .doOnNext(ComplexFlowApp::auditResealed // pierzi CANCEL signal
-        // daca subscriberu final da cancel audit resealed deja lansate nu poti sa le cancelezi.
-               )
-            ;
-       // Q de la biz: nu ne pasa de erorile de la audit
-      // problema: auditu dureaza si nu are sens sa stam dupa el. Sa facem deci fire-and-forget
-   }
-
-   private static void auditResealed(Product p) {
-      if (p.isResealed()) {
-          ExternalAPIs.auditResealedProduct(p)
-                 .subscribe(Utils::noop, Utils::handleError);
-      }
-   }
-
-   // perfect
-   private static Flux<Product> retrieveMany(List<Long> productIds) {
-      log.info("Call pentru " + productIds);
-      return WEB_CLIENT
-              .post()
-              .uri("http://localhost:9999/api/product/many")
-              .bodyValue(productIds)
-              .retrieve()
-              .bodyToFlux(ProductDetailsResponse.class) // jackson parseaza progresiv JSONu cum vine si-ti emite semnale de date ProductDetails.
-              .map(ProductDetailsResponse::toEntity)
-      ;
-   }
-
+                .doOnNext(ComplexFlowApp::auditResealed // pierzi CANCEL signal
+                        // daca subscriberu final da cancel audit resealed deja lansate nu poti sa le cancelezi.
+                )
+                ;
+        // Q de la biz: nu ne pasa de erorile de la audit
+        // problema: auditu dureaza si nu are sens sa stam dupa el. Sa facem deci fire-and-forget
+    }
 
 
 }
