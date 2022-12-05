e98df2a66526e36dc3f9ae49447921ebb2b02fac
Victor Rentea <victorrentea@gmail.com>
2022-12-05 10:43:55 +0200

    Wiremock nonblocant

--- spring-webflux/src/main/java/victor/training/reactive/StartWireMock.java
+++ spring-webflux/src/main/java/victor/training/reactive/StartWireMock.java
@@ -5,11 +5,12 @@ import com.github.tomakehurst.wiremock.standalone.WireMockServerRunner;
 import java.io.File;
 
 public class StartWireMock {
-   public static void main(String[] args) {
-      WireMockServerRunner.main(
-          "--port", "9999",
-          "--root-dir", new File(".", "spring-webflux/src/test/resources").getAbsolutePath(),
-          "--global-response-templating" // UUID
-      );
-   }
+  public static void main(String[] args) {
+    WireMockServerRunner.main(
+            "--port", "9999",
+            "--root-dir", new File(".", "spring-webflux/src/test/resources").getAbsolutePath(),
+            "--global-response-templating", // UUID
+            "--async-response-enabled=true" // enable Wiremock to not bottleneck on heavy load
+    );
+  }
 }
