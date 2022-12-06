7c776a8a53fdfb54ae9f7592247885deee31ec6a
Victor Rentea <victorrentea@gmail.com>
2022-12-06 03:49:24 +0200

    add websockets dry run

--- spring-webflux/src/main/java/victor/training/reactive/usecase/complex/ExceptionInterceptor.kt
+++ spring-webflux/src/main/java/victor/training/reactive/usecase/complex/ExceptionInterceptor.kt
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@RestControllerAdvice
-class ExceptionInterceptor {
-    private val log = LoggerFactory.getLogger(ExceptionInterceptor::class.java)
-
-    @ExceptionHandler(Exception::class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun serverExceptionHandler(ex: Exception): String {
-        log.error(ex.message, ex)
-        return "Oups: " + ex.message
-    }
-}
+//@RestControllerAdvice
+//class ExceptionInterceptor {
+//    private val log = LoggerFactory.getLogger(ExceptionInterceptor::class.java)
+//
+//    @ExceptionHandler(Exception::class)
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    fun serverExceptionHandler(ex: Exception): String {
+//        log.error(ex.message, ex)
+//        return "Oups: " + ex.message
+//    }
+//}
