0ee7190f8f5c4e95a1b173324018918d3036a22d
Victor Rentea <victorrentea@gmail.com>
2022-12-13 12:29:19 +0200

    cherry pick this

--- spring-webflux/src/main/java/victor/training/reactive/demo/spring/HotPublisher.java
+++ spring-webflux/src/main/java/victor/training/reactive/demo/spring/HotPublisher.java
@@ -12,14 +12,14 @@ import java.util.Objects;
 
 @RestController
 public class HotPublisher {
-   // publisher cold
    private static final Flux<Long> coldFlux = Flux.interval(Duration.ofSeconds(1));
    @GetMapping(value = "tick/cold", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
    public Flux<String> cold() {
-      return coldFlux.map(o -> "Au trecut "+ o + " secunde");
+      return coldFlux
+              .log()
+              .map(o -> o + "s passed since your connection");
    }
 
-   // publisher hot
    private ConnectableFlux<Long> hotFlux;
    @PostConstruct
    public void initHotFlux() {
@@ -28,7 +28,8 @@ public class HotPublisher {
    }
    @GetMapping(value = "tick/hot", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
    public Flux<String> hot() {
-      return hotFlux.map(Objects::toString);
+      return hotFlux.log()
+              .map(o -> o + "s passed since the .connect()");
    }
 
 
