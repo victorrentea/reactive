6bd664433edee0019a8105676722e2be3cc501f6
Victor Rentea <victorrentea@gmail.com>
2022-12-13 12:18:40 +0200

    cold care dropuie elementele venite  dela publisher in exces (mai mult de cat a cerut)

--- reactor-lite/src/main/java/victor/training/reactor/workshop/BackPressure.java
+++ reactor-lite/src/main/java/victor/training/reactor/workshop/BackPressure.java
@@ -11,24 +11,24 @@ import java.time.Duration;
 public class BackPressure {
   public static void main(String[] args) {
     //https://stackoverflow.com/questions/57296097/how-does-backpressure-work-in-project-reactor
-    System.out.println("------ Cold publisher that respects the request(n) from subscriber");
-    // asa ar fi corect!!
-    Flux.range(1, Integer.MAX_VALUE)
-            .log()
-            .concatMap(x -> Mono.delay(Duration.ofMillis(100)), 2) // simulate that processing takes time
-            .take(4)
-            .blockLast();
-
-
-
-//    System.out.println("------ Cold publisher that does NOT respect the request(n) from subscriber");
-//    Flux.interval(Duration.ofMillis(50))
-//            // .onBackpressureBuffer()
-//            // .onBackpressureDrop()
+//    System.out.println("------ Cold publisher that respects the request(n) from subscriber");
+//    // asa ar fi corect!!
+//    Flux.range(1, Integer.MAX_VALUE)
 //            .log()
-//            .concatMap(x -> Mono.delay(Duration.ofMillis(100)), 1) // simulate that processing takes time
+//            .concatMap(x -> Mono.delay(Duration.ofMillis(100)), 2) // simulate that processing takes time
 //            .take(4)
 //            .blockLast();
+
+
+
+    System.out.println("------ Cold publisher that does NOT respect the request(n) from subscriber");
+    Flux.interval(Duration.ofMillis(50)) // impinge la fiecare 50 ms cate un element in jos... FARA sa-i pese de cate ai cerut,
+//            .onBackpressureBuffer()
+            .onBackpressureDrop()
+            .log()
+            .concatMap(x -> Mono.delay(Duration.ofMillis(100)), 1) // simulate that processing takes time
+            .take(4)
+            .blockLast();
 
 
 //    ConnectableFlux<Long> hotFlux = Flux.interval(Duration.ofMillis(10))
