7b14340e42e4f24b01a591e61104a08d17528bdf
Victor Rentea <victorrentea@gmail.com>
2023-01-15 23:23:53 +0200

    yolo

--- reactor/src/test/java/victor/training/reactor/workshop/solved/P5_TestingTestSolved.java
+++ reactor/src/test/java/victor/training/reactor/workshop/solved/P5_TestingTestSolved.java
@@ -1,7 +1,6 @@
 package victor.training.reactor.workshop.solved;
 
 import org.junit.jupiter.api.BeforeEach;
-import victor.training.reactor.workshop.P5_TestingSolved;
 import victor.training.reactor.workshop.P5_TestingTest;
 
 public class P5_TestingTestSolved extends P5_TestingTest {
