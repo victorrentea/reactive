ac041b0d38bfc2b0a0e5dd0eb4f69f5e0bad3341
Victor Rentea <victorrentea@gmail.com>
2023-06-08 11:28:47 +0300

    webclient

--- spring-mvc/src/main/java/victor/training/reactive/intro/mvc/BlockingApp.java
+++ spring-mvc/src/main/java/victor/training/reactive/intro/mvc/BlockingApp.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -27,145 +27,157 @@ import static victor.training.reactive.intro.mvc.Utils.sleep;
 @SpringBootApplication
 public class BlockingApp {
 
-   private static final Logger log = LoggerFactory.getLogger(BlockingApp.class);
+  private static final Logger log = LoggerFactory.getLogger(BlockingApp.class);
 
-   @Bean
-   MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-      // expose tomcat metrics (threads, connections) to prometheus via /actuator/prometheus
-      return registry -> registry.config().commonTags(
-              "application", "spring-mvc",
-              "region", "training-region");
-   }
+  @Bean
+  MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+    // expose tomcat metrics (threads, connections) to prometheus via /actuator/prometheus
+    return registry -> registry.config().commonTags(
+        "application", "spring-mvc",
+        "region", "training-region");
+  }
 
 
-   public static void main(String[] args) {
-      org.springframework.boot.SpringApplication.run(BlockingApp.class, args);
-   }
+  public static void main(String[] args) {
+    org.springframework.boot.SpringApplication.run(BlockingApp.class, args);
+  }
 
-   @Autowired
-   private Barman barman;
+  @Autowired
+  private Barman barman;
 
-   @GetMapping("fast")
-   public String fast() {
-      return "immediate";
-   }
+  @GetMapping("fast")
+  public String fast() {
+    return "immediate";
+  }
 
-   @PostMapping
-   public void acceptPayment(Double baniiClientului) {
+  @PostMapping
+  public void acceptPayment(Double baniiClientului) {
 
-   }
+  }
 
-   @Autowired
-   private  ThreadPoolTaskExecutor barPool; // MUST HAVE pt ca spring asa propaga SecurityContext, TraceID
-   // shutdown cand opresti app.
+  @Autowired
+  private ThreadPoolTaskExecutor barPool; // MUST HAVE pt ca spring asa propaga SecurityContext, TraceID
+  // shutdown cand opresti app.
 
-   @GetMapping("drink")
-   public CompletableFuture<DillyDilly> drink() throws Exception {
-      log.info("Talking to barman: " + barman.getClass());
+  @GetMapping("drink")
+  public CompletableFuture<DillyDilly> drink() throws Exception {
+    log.info("Talking to barman: " + barman.getClass());
 
-      long t0 = currentTimeMillis();
+    long t0 = currentTimeMillis();
 
-      // promise, deferred === CompletableFuture
-      CompletableFuture<Beer> beerPromise = CompletableFuture.supplyAsync(() -> barman.pourBeer(), barPool);
-      CompletableFuture<Vodka> vodkaPromise = CompletableFuture.supplyAsync(() -> barman.pourVodka(), barPool);
+    // promise, deferred === CompletableFuture
+    CompletableFuture<Beer> beerPromise = barman.pourBeer();
+    CompletableFuture<Vodka> vodkaPromise = CompletableFuture.supplyAsync(() -> barman.pourVodka(), barPool);
 
-      CompletableFuture<DillyDilly> dillyPromise = beerPromise.thenCombine(vodkaPromise, (beer, vodka) ->
-          barman.mixCocktail(beer, vodka));
+    CompletableFuture<DillyDilly> dillyPromise = beerPromise.thenCombine(vodkaPromise, (beer, vodka) ->
+        barman.mixCocktail(beer, vodka));
 
-      log.debug("HTTP thread was blocked for {} millis ", (currentTimeMillis() - t0));
-      return dillyPromise;
-      // nu ma blochez eu dupa rezultat ci ii dau promiseul lui spring, care
-      // va agata un callback eg .thenAccept(dilly- > .....)
-      // prin care va scrie pe HTTP response ce a venit
-   }
+    log.debug("HTTP thread was blocked for {} millis ", (currentTimeMillis() - t0));
+    return dillyPromise;
+    // nu ma blochez eu dupa rezultat ci ii dau promiseul lui spring, care
+    // va agata un callback eg .thenAccept(dilly- > .....)
+    // prin care va scrie pe HTTP response ce a venit
+  }
 
 }
+
 @Configuration
 class Config {
-   @Bean
-   public ThreadPoolTaskExecutor barPool(@Value("${bar.pool.size}")int barPoolSize) {
-      ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-      executor.setCorePoolSize(barPoolSize);
-      executor.setMaxPoolSize(barPoolSize);
-      executor.setQueueCapacity(500);
-      executor.setThreadNamePrefix("bar-");
-      executor.initialize();
-      executor.setWaitForTasksToCompleteOnShutdown(true);
-      return executor;
-   }
+  @Bean
+  public ThreadPoolTaskExecutor barPool(@Value("${bar.pool.size}") int barPoolSize) {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(barPoolSize);
+    executor.setMaxPoolSize(barPoolSize);
+    executor.setQueueCapacity(500);
+    executor.setThreadNamePrefix("bar-");
+    executor.initialize();
+    executor.setWaitForTasksToCompleteOnShutdown(true);
+    return executor;
+  }
 }
 
 @Service
 class Barman {
-   private static final Logger log = LoggerFactory.getLogger(Barman.class);
+  private static final Logger log = LoggerFactory.getLogger(Barman.class);
 
-   public Beer pourBeer() {
-      log.info("Start beer");
-      // 2: blocking REST call
-      Beer beer = new RestTemplate().getForEntity("http://localhost:9999/api/beer", Beer.class).getBody();
-      log.info("End beer");
-      return beer;
-   }
+  public CompletableFuture<Beer> pourBeer() {
+    log.info("Start beer");
+    // 2: blocking REST call
+    // linia de mai jos tine 1 MB de RAM ocupat pt 1 sec degeaba.
+//      Beer beer = new RestTemplate().getForEntity("http://localhost:9999/api/beer", Beer.class).getBody();
+     CompletableFuture<Beer> beerFuture = WebClient.create().get()
+         .uri("http://localhost:9999/api/beer")
+         .retrieve()
+         .bodyToMono(Beer.class)
+         .toFuture();
+     log.info("End beer");
+    return beerFuture;
+  }
 
-   public Vodka pourVodka() {
-      log.info("Start vodka");
-      sleep(200);  // imagine blocking DB call
-      log.info("End vodka");
-      return new Vodka();
-   }
+  public Vodka pourVodka() {
+    log.info("Start vodka");
+    sleep(200);  // imagine blocking DB call
+    log.info("End vodka");
+    return new Vodka();
+  }
 
 
-   public DillyDilly mixCocktail(Beer beer, Vodka vodka) {
-      log.info("Mixing {} with {} (takes time) ...", beer, vodka);
-      sleep(500);
-      return new DillyDilly(beer, vodka);
-   }
+  public DillyDilly mixCocktail(Beer beer, Vodka vodka) {
+    log.info("Mixing {} with {} (takes time) ...", beer, vodka);
+    sleep(500);
+    return new DillyDilly(beer, vodka);
+  }
 }
 
 
 class Beer {
-   private String type;
-   public Beer() {}
-   public Beer(String type) {
-      this.type = type;
-   }
-   public String getType() {
-      return type;
-   }
+  private String type;
+
+  public Beer() {
+  }
+
+  public Beer(String type) {
+    this.type = type;
+  }
+
+  public String getType() {
+    return type;
+  }
 }
 
 class Vodka {
-   private final String type = "deadly";
-   public String getType() {
-      return type;
-   }
+  private final String type = "deadly";
+
+  public String getType() {
+    return type;
+  }
 }
 
 class DillyDilly {
-   private static final Logger log = LoggerFactory.getLogger(DillyDilly.class);
-   private final Beer beer;
-   private final Vodka vodka;
+  private static final Logger log = LoggerFactory.getLogger(DillyDilly.class);
+  private final Beer beer;
+  private final Vodka vodka;
 
-   public DillyDilly(Beer beer, Vodka vodka) {
-      this.beer = beer;
-      this.vodka = vodka;
-   }
+  public DillyDilly(Beer beer, Vodka vodka) {
+    this.beer = beer;
+    this.vodka = vodka;
+  }
 
-   public Beer getBeer() {
-      return beer;
-   }
+  public Beer getBeer() {
+    return beer;
+  }
 
-   public Vodka getVodka() {
-      return vodka;
-   }
+  public Vodka getVodka() {
+    return vodka;
+  }
 }
 
 class Utils {
-   public static void sleep(long millis) {
-      try {
-         Thread.sleep(millis);
-      } catch (InterruptedException e) {
-         throw new RuntimeException(e);
-      }
-   }
+  public static void sleep(long millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
